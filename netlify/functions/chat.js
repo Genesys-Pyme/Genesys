b0be599b7fc816dcba5681fc650d6679c74be81a
Javier Castillo <javiercastillo.tuc@gmail.com>
2026-03-11 11:02:08 -0300

    subir

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -147,6 +147,9 @@ console.log("Error analizando lead:",err)
 
 await fetch("https://script.google.com/macros/s/AKfycbxgZDRJZ3QRqdC6N6TtO15QoDGPCFK8HZvhBQPW6kfmtR-t5prkT9Wt5wWu-eDu-Io/exec",{
 method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
 body:JSON.stringify({
 negocio:lead.negocio,
 tieneWeb:lead.tieneWeb,
